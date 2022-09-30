09830df95c7c5714847919197608e3462f3f1d61
hiccuoboi <denis.shulimovv@gmail.com>
2022-09-30 16:52:30 +0300

    event button update

--- gui.py
+++ gui.py
@@ -9,7 +9,14 @@ class DlgMain(QDialog):
         self.resize(300, 200)
 
         self.ledText = QLineEdit('DefaultText', self)
-        self.ledText.move(100, 50)
+        self.ledText.move(75, 50)
+
+        self.btnUpdate = QPushButton('Update Window Title', self)
+        self.btnUpdate.move(80, 80)
+        self.btnUpdate.clicked.connect(self.evt_btn_update_clicked)
+
+    def evt_btn_update_clicked(self):
+        self.setWindowTitle(self.ledText.text())
 
 
 if __name__ == '__main__':
